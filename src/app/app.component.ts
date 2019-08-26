93d8a8d1fdaf25209273582d3b8c900d98595030
jorgebran2002 <jorgebran2002@gmail.com>
2019-08-25 21:01:33 -0600

    Ejemplos de PIPES en angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent
+ {
+  
   title = 'pipes';
+
+  nombre="Jorge";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
 }
